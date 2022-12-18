f7698f77e2ba23b45e7df909d5b8c75ec693e602
EC2 Default User <ec2-user@ip-172-31-92-38.ec2.internal>
2022-12-18 13:55:59 +0000

    bug fix in response parsing

--- test_serve_cifar.py
+++ test_serve_cifar.py
@@ -1,12 +1,14 @@
 import requests
 import os
+import json 
 
-for image in os.listdir(images):
+for image in os.listdir('./images'):
     response = requests.post('http://127.0.0.1:8080/predictions/cifar10',
                              files={'data': open(f'images/{image}', 'rb')}
                             )
     if response.status_code == 200:
-        d = dict(response.text)
+        print(f'response text: {response.text}')
+        d = dict(json.loads(response.text))
         for k, v in d.items():
             print(k, v)
     else:
