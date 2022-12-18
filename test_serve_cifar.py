84231c9ac3c33c0cefd7023fc2b7aede557f9c9e
Darshat <darshat@outlook.com>
2022-12-18 19:29:04 +0530

    Better output message

--- test_serve_cifar.py
+++ test_serve_cifar.py
@@ -7,9 +7,9 @@ for image in os.listdir('./images'):
                              files={'data': open(f'images/{image}', 'rb')}
                             )
     if response.status_code == 200:
-        print(f'response text: {response.text}')
         d = dict(json.loads(response.text))
+        print(f'For image {image}')
         for k, v in d.items():
-            print(k, v)
+            print(f'\toutput {k}:{v}')
     else:
         print(f'Error with request: {response.status_code}')
